a3ee3c49517d5fb617644d898f83a29f01e9784c
stas_chik <stas.chikvarov@gmail.com>
2013-03-21 22:23:14 +0400

    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  
+  resources :users
+  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
@@ -8,6 +9,7 @@ SampleApp::Application.routes.draw do
   match '/signup',  :to => 'users#new'
 
   root :to => 'pages#home'  
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
